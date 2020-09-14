00b38fbc7e113d3ec5886a8d8e6fa144196f6121
bazzl1202 <bazzl1202@wrdsb.ca>
2020-09-14 15:33:22 +0000

    Now calculates quotient and remainder.

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -10,6 +11,28 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+
+    //declare variable that stores first integer
+    int firstInteger;
+
+    //declare variable that stores second integer
+    int secondInteger;
+
+    //ask the user for two integers
+    System.out.println("Please enter in two integers, on seperate lines, to divide.");
+
+    //initialize the users two integers
+    firstInteger = input.nextInt();
+    secondInteger = input.nextInt();
+
+    // declare and initialize quotient
+    int quotient = firstInteger/secondInteger;
+
+    //declare and initialize modulus 
+    int modulus = firstInteger%secondInteger;
+
+    // tell the user the quotient and remainder
+    System.out.println(firstInteger + "/" + secondInteger + " is " + quotient + " with a remainder of "+ modulus + ".");
   }
 }
